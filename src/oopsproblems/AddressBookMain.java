dab93640779495b0350e612ecdf123b60b657061
Veer1498 <singavarapuveerendra@gmail.com>
2022-06-30 13:20:02 +0530

    Welcome Message

---
+++ src/oopsproblems/AddressBookMain.java
@@ -0,0 +1,20 @@
+package oopsproblems;
+/**
+ * 
+ * 
+ * @author singa
+ *
+ */
+public class AddressBookMain {
+/**
+ * 
+ * 
+ * @param args
+ */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Welcome to Address Book Program");
+
+	}
+
+}
